92495729843a59341e248f476fc386e64b2ed1cf
sriharieetyala <sriharieetyala143@gmail.com>
2026-01-04 23:27:25 +0530

    create footer component with social links

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './footer.html',
-  styleUrl: './footer.css',
+  styleUrl: './footer.css'
 })
 export class Footer {
-
+  currentYear = new Date().getFullYear();
 }
